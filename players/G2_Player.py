1602f868675dd529da22e4e201df9c87d5be698a
hanita <hhaller@wisc.edu>
2024-10-28 16:19:28 -0400

    even cuts code

--- players/G2_Player.py
+++ players/G2_Player.py
@@ -7,6 +7,8 @@ import miniball
 from enum import Enum
 
 from piece_of_cake_state import PieceOfCakeState
+from players.g2.helpers import *
+from players.g2.even_cuts import *
 
 
 class Strategy(Enum):
@@ -37,12 +39,11 @@ class G2_Player:
         self.logger = logger
         self.tolerance = tolerance
         self.cake_len = None
-        self.move_queue = []
+        self.cake_width = None
         self.move_queue = []
 
-        self.phase = "HORIZONTAL"
-        self.direction = ""
         self.strategy = Strategy.SNEAK
+        self.move_object = None
 
     def cut(self, cake_len, cake_width, cur_pos) -> tuple[int, List[int]]:
         if cur_pos[0] == 0:
@@ -77,106 +78,6 @@ class G2_Player:
             )
 
         return constants.ASSIGN, assignment
-
-    def sneak(self, start_pos, end_pos, cake_width, cake_len):
-        """
-        Given a start position & goal position, uses the 1-pixel shaving technique
-        to append the necessary steps to the move_queue
-        """
-        nearest_x, x_dist = nearest_edge_x(start_pos, cake_width)
-        nearest_y, y_dist = nearest_edge_y(start_pos, cake_len)
-
-        end_x, end_x_dist = nearest_edge_x(end_pos, cake_width)
-        end_y, end_y_dist = nearest_edge_y(end_pos, cake_len)
-
-        # if we are on the top or bottom or the board and require more than 1 move
-        if y_dist == 0 and (x_dist > 0.1 or nearest_x != end_x):
-            bounce_y = bounce(nearest_y)
-            self.move_queue.append([end_x, bounce_y])
-
-            # if the end is not on the same line so we must ricochet off of corner
-            if end_y_dist > 0 or nearest_y != end_y:
-                bounce_x = bounce(end_x)
-                self.move_queue.append([bounce_x, nearest_y])
-
-                # if the end position is on the opposite side
-                if end_y_dist == 0:
-                    bounce_y = bounce(end_y)
-                    self.move_queue.append([bounce_x, end_y])
-                    self.move_queue.append([end_x, bounce_y])
-
-        # if we are on the left or right side of the board and require more than 1 move
-        elif x_dist == 0 and (y_dist > 0.1 or nearest_y != end_y):
-            bounce_x = bounce(nearest_x)
-            self.move_queue.append([bounce_x, end_y])
-
-            # if the end is not on the same line so we must ricochet off of corner
-            if end_x_dist > 0 or nearest_x != end_x:
-                bounce_y = bounce(end_y)
-                self.move_queue.append([nearest_x, bounce_y])
-
-                # if the end position is on the opposite side
-                if end_x_dist == 0:
-                    bounce_x = bounce(end_x)
-                    self.move_queue.append([end_x, bounce_y])
-                    self.move_queue.append([bounce_x, end_y])
-
-        self.move_queue.append(end_pos)
-        return
-
-    def even_cuts(self):
-        """
-        Adds moves to the merge queue that will cut the cake into even slices.
-        """
-
-        n = len(self.requests)
-        s_x = self.cake_width / np.sqrt(n)
-        s_y = self.cake_len / np.sqrt(n)
-        pos = self.cur_pos
-
-        if self.turn_number == 2:
-            self.move_queue.append([0, s_y])
-            self.move_queue.append([self.cake_width, s_y])
-            return
-
-        if self.phase == "HORIZONTAL" and pos[1] + s_y >= self.cake_len:
-            self.phase = "VERTICAL"
-            if pos[0] == 0:
-                new_x = s_x
-            else:
-                new_x = self.cake_width - s_x
-                self.direction = "RIGHT"
-            self.sneak(pos, [new_x, self.cake_len], self.cake_width, self.cake_len)
-            self.move_queue.append([new_x, 0])
-
-            return
-
-        if self.phase == "HORIZONTAL":
-            self.sneak(pos, [pos[0], pos[1] + s_y], self.cake_width, self.cake_len)
-            if pos[0] == 0:
-                opposite = self.cake_width
-            else:
-                opposite = 0
-            self.move_queue.append([opposite, round(pos[1] + s_y, 2)])
-
-        else:
-            if self.direction == "RIGHT":
-                new_x = pos[0] - s_x
-            else:
-                new_x = pos[0] + s_x
-
-            if new_x <= 0 or new_x >= self.cake_width:
-                self.phase = "DONE"
-                return
-
-            self.sneak(pos, [new_x, pos[1]], self.cake_width, self.cake_len)
-            if pos[1] == 0:
-                opposite = self.cake_len
-            else:
-                opposite = 0
-            self.move_queue.append([new_x, opposite])
-
-        return
 
     def can_cake_fit_in_plate(self, cake_piece, radius=12.5):
         cake_points = np.array(
@@ -226,35 +127,6 @@ class G2_Player:
                     penalty += penalty_percentage
         return penalty
 
-    def sneak_alg(self):
-        if self.turn_number == 1:
-            return constants.INIT, [0.01, 0]
-
-        if len(self.move_queue) == 0 and self.phase != "DONE":
-            self.even_cuts()
-
-        if len(self.move_queue) > 0:
-            next_val = self.move_queue.pop(0)
-            cut = [round(next_val[0], 2), round(next_val[1], 2)]
-            return constants.CUT, cut
-
-        if len(self.polygons) < len(self.requests):
-            if self.cur_pos[0] == 0:
-                return constants.CUT, [
-                    self.cake_width,
-                    round((self.cur_pos[1] + 5) % self.cake_len, 2),
-                ]
-            else:
-                return constants.CUT, [
-                    0,
-                    round((self.cur_pos[1] + 5) % self.cake_len, 2),
-                ]
-
-        if len(self.polygons) < len(self.requests):
-            return self.cut(self.cake_len, self.cake_width, self.cur_pos)
-        else:
-            return self.assign(self.polygons, self.requests)
-
     def process_percept(self, current_percept: PieceOfCakeState):
         self.polygons = current_percept.polygons
         self.turn_number = current_percept.turn_number
@@ -270,46 +142,16 @@ class G2_Player:
         current_penalty = self.__calculate_penalty()
         print(f"current penalty: {current_penalty}")
 
-        match self.strategy:
-            case Strategy.SNEAK:
-                return self.sneak_alg()
+        if self.strategy == Strategy.SNEAK:
+            if self.turn_number == 1:
+                self.move_object = EvenCuts(len(self.requests), self.cake_width, self.cake_len)
+            
+            move = self.move_object.move(self.turn_number, self.cur_pos)
+            if move == None:
+                self.assign(self.polygons, self.requests)
 
-            case _:
-                return self.sneak_alg()
+            return move
+        
+        else:
+            return self.assign(self.polygons, self.requests)
 
-
-def nearest_edge_x(pos, cake_width):
-    """
-    Returns the nearest X-edge and the distance to said edge
-    X-edge is 0 if the position is closer to the left side, or cake_width
-        if it is closer to the right side.
-    """
-    min_x = pos[0]
-    x_edge = 0
-    if cake_width - pos[0] < min_x:
-        min_x = cake_width - pos[0]
-        x_edge = cake_width
-    return x_edge, min_x
-
-
-def nearest_edge_y(pos, cake_len):
-    """
-    Returns the nearest Y-edge and the distance to said edge
-    Y-edge is 0 if the position is closer to the top, or cake_len
-        if it is closer to the bottom.
-    """
-    min_y = pos[1]
-    y_edge = 0
-    if cake_len - pos[1] < min_y:
-        min_y = cake_len - pos[1]
-        y_edge = cake_len
-    return y_edge, min_y
-
-
-def bounce(margin):
-    """
-    Returns a value 0.01 away from the provided margin
-    """
-    if margin == 0:
-        return 0.01
-    return round(margin - 0.01, 2)
