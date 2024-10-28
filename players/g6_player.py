1096c1fc6aa8ccae13b79c1ee116df64d0e16b0f
Adithi Narayan <adithin2001@gmail.com>
2024-10-28 09:55:36 -0400

    angles and direction vectors

--- players/g6_player.py
+++ players/g6_player.py
@@ -4,7 +4,7 @@ from typing import List
 
 import numpy as np
 import logging
-
+import traceback
 import constants
 
 
@@ -42,7 +42,8 @@ class Player:
         self.cake_len = None
         self.cake_width = None
         self.cuts = set()
-
+        self.cutList =[]
+        self.requests = None
 
     # Move the knife to the side in the clockwise direction
     def move_knife_clockwise(self, cur_pos):
@@ -89,6 +90,90 @@ class Player:
             while [cur_pos[0], cur_pos[1], self.cake_width, val] in self.cuts or [self.cake_width, val, cur_pos[0], cur_pos[1]] in self.cuts:
                 val -= 0.01
             return [self.cake_width, val]
+    
+    def move_straight(self,cur_pos, dir='R'):
+        switch ={
+            'L': (0,cur_pos[1]),
+            'R': (self.cake_width, cur_pos[1]),
+            'U': (cur_pos[0], 0),
+            'D': (cur_pos[0], self.cake_len)
+        }
+        return switch.get(dir)
+    
+    def move_angle(self, cur_pos, deg):
+        deg = deg % 360
+        theta_radians = np.radians(deg)
+
+        dx = np.cos(theta_radians)
+        dy = np.sin(theta_radians)
+
+        intersection = None
+        intersection = self.check_intersections(dx, dy, cur_pos)
+
+        if intersection is None:
+            # Take 180-angle i.e. line other side
+            dx = np.cos(theta_radians + np.pi)
+            dy = np.sin(theta_radians + np.pi)
+            intersection = self.check_intersections(dx, dy, cur_pos)
+
+        return intersection
+
+    def check_intersections(self, dx, dy, cur_pos):
+        L, W = self.cake_width, self.cake_len
+        x0, y0 = cur_pos
+        intersection = None
+
+        # left side (x = 0)
+        if dx != 0:
+            tl = -x0 / dx
+            if tl >= 0:
+                y = round(y0 + tl * dy,2)
+                if 0 <= y <= W and (0, y) != cur_pos:
+                    intersection = (0, y)
+
+        #  right side (x = L)
+        if dx != 0:
+            tr = (L - x0) / dx
+            if tr >= 0:
+                y = round(y0 + tr * dy,2)
+                if 0 <= y <= W and (L, y) != cur_pos:
+                    if intersection is None or tr < tl:
+                        intersection = (L, y)
+
+        # down side (y = 0)
+        if dy != 0:
+            td = -y0 / dy
+            if td >= 0:
+                x = round(x0 + td * dx,2)
+                if 0 <= x <= L and (x, 0) != cur_pos:
+                    if intersection is None or td < min(tl, tr):
+                        intersection = (x, 0)
+
+        #  up side (y = W)
+        if dy != 0:
+            tu = (W - y0) / dy
+            if tu >= 0:
+                x = round(x0 + tu * dx,2)
+                if 0 <= x <= L and (x, W) != cur_pos:
+                    if intersection is None or tu < min(tl, tr):
+                        intersection = (x, W)
+
+        return intersection
+
+    def make_cuts(self):
+        print(self.cake_len, self.cake_width)
+        n = len(self.requests)
+        angles = [i * (360 / n) for i in range(n)]
+        start = (0,round(self.cake_width/2,2))
+        print(angles)
+        self.cutList.append(start)
+        for i in range(n):
+            start = self.move_angle(start, angles[i])
+            self.cutList.append(start)
+            print(start)
+
+
+        
 
     def move(self, current_percept) -> (int, List[int]):
         """Function which retrieves the current state of the cake
@@ -108,20 +193,31 @@ class Player:
         requests = current_percept.requests
         self.cake_len = current_percept.cake_len
         self.cake_width = current_percept.cake_width
+        self.requests = requests
 
-        if turn_number == 1:
-            return constants.INIT, [0,0]
+        try:
+            if turn_number == 1:
+                self.make_cuts()
+                init = self.cutList.pop(0)
+                return constants.INIT, [init[0],init[1]]
 
-        if len(polygons) < len(requests):
-            if cur_pos[0] == 0:
-                return constants.CUT, [self.cake_width, round((cur_pos[1] + 5)%self.cake_len, 2)]
-            else:
-                return constants.CUT, [0, round((cur_pos[1] + 5)%self.cake_len, 2)]
+            while self.cutList:
+                next_move = self.cutList.pop(0)
+                return constants.CUT, [next_move[0], next_move[1]]
 
-        # Assign the pieces
-        assignment = []
-        for i in range(len(requests)):
-            assignment.append(i)
+            if len(polygons) < len(requests):
+                if cur_pos[0] == 0:
+                    return constants.CUT, [self.cake_width, round((cur_pos[1] + 5)%self.cake_len, 2)]
+                else:
+                    return constants.CUT, [0, round((cur_pos[1] + 5)%self.cake_len, 2)]
 
+            # Assign the pieces
+            assignment = []
+            for i in range(len(requests)):
+                assignment.append(i)
+            return constants.ASSIGN, assignment
+        
+        except Exception as e:
+            print(e)
+            traceback.print_exc()
 
-        return constants.ASSIGN, assignment
