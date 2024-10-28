86b6db539e35dbf77ffc4d4d864af5bed23cdfe7
Julianna Yu <julianna-yu@github.com>
2024-10-27 23:24:58 -0400

    working move, debugging matching pieces to requests

---
+++ players/group10_player.py
@@ -0,0 +1,135 @@
+from collections import defaultdict
+import os
+import pickle
+from typing import List
+
+import numpy as np
+import math
+import logging
+
+import constants
+
+
+class Player:
+    def __init__(self, rng: np.random.Generator, logger: logging.Logger,
+                 precomp_dir: str, tolerance: int) -> None:
+        """Initialise the player with the basic information
+
+            Args:
+                rng (np.random.Generator): numpy random number generator, use this for same player behavior across run
+                logger (logging.Logger): logger use this like logger.info("message")
+                precomp_dir (str): Directory path to store/load pre-computation
+                tolerance (int): tolerance for the cake distribution
+                cake_len (int): Length of the smaller side of the cake
+        """
+
+        self.rng = rng
+        self.logger = logger
+        self.tolerance = tolerance
+        self.cake_len = None
+        self.cake_width = None
+        self.cake_diagonal = None
+        self.requests = None
+        self.cuts = []
+
+    def move(self, current_percept) -> (int, List[int]):
+        """Function which retrieves the current state of the amoeba map and returns an amoeba movement
+
+            Args:
+                current_percept(TimingMazeState): contains current state information
+            Returns:
+                int: This function returns the next move of the user:
+                    WAIT = -1
+                    LEFT = 0
+                    UP = 1
+                    RIGHT = 2
+                    DOWN = 3
+        """
+        polygons = current_percept.polygons
+        turn_number = current_percept.turn_number
+        cut_number = current_percept.turn_number - 1
+        cur_pos = current_percept.cur_pos
+    
+        if turn_number == 1:
+            # initialize instance variables, sorted requests
+            self.requests = current_percept.requests
+            self.cake_len = current_percept.cake_len
+            self.cake_width = current_percept.cake_width
+            self.cake_diagonal = self.calcDiagonal()
+            print ("cake_len:", self.cake_len)
+            print ("cake_width:", self.cake_width)
+            print ("cake_diagonal:", self.cake_diagonal)
+            self.cuts.append((0, 0))
+            return constants.INIT, [0,0]
+
+        
+        # case if the diagonal of the total cake is <= 25
+        if self.cake_diagonal <= 25:
+            # assign pieces
+            if (cut_number > len(self.requests)):
+                assignment = self.assignPolygons(polygons=polygons)
+                return constants.ASSIGN, assignment
+            current_area = self.requests[cut_number - 1]
+            if (cut_number == 1):
+                x = round(2 * current_area / self.cake_len, 2)
+                y = self.cake_len
+                self.cuts.append((x, y))
+                return constants.CUT, [x, y]
+            else:
+                x = round(self.cuts[cut_number - 2][0] + (2 * current_area / self.cake_len), 2)
+                y = (0, self.cake_len) [cut_number % 2]
+                self.cuts.append((x, y))
+                return constants.CUT, [x, y]
+            
+        else:
+            if len(polygons) != len(self.requests):
+                if cur_pos[0] == 0:
+                    return constants.CUT, [self.cake_width, round((cur_pos[1] + 5)%self.cake_len, 2)]
+                else:
+                    return constants.CUT, [0, round((cur_pos[1] + 5)%self.cake_len, 2)]
+
+            assignment = []
+            for i in range(len(self.requests)):
+                assignment.append(i)
+
+            return constants.ASSIGN, assignment
+    
+    def calcDiagonal(self):
+        return (math.sqrt((self.cake_len * self.cake_len) + (self.cake_width * self.cake_width)))
+    
+    def assignPolygons(self, polygons):
+        # parse polygons to polygon_areas: dict(rank: (area, i))
+        polygon_areas = []
+        requests_items = []
+        for i in range(len(polygons)):
+            polygon_areas.append((polygons[i].area, i)) 
+        for i in range(len(self.requests)):
+            requests_items.append((self.requests[i], i))
+        matches = {} # polygon : request
+        for i in range(len(requests_items)):
+            min_diff = math.inf
+            polygon = -1
+            polygon_index = -1
+            for j in range(len(polygon_areas)):
+                if math.abs(polygon_areas[j][0] - requests_items[i][0]) < min_diff:
+                    min_diff = math.abs(polygon_areas[j][0] - requests_items[i][0])
+                    polygon = polygon_areas[j][1]
+                    polygon_index = j
+            print ("polygon:", polygon)
+            assignment[polygon] = i
+            polygon_areas.pop(polygon_index)
+        
+        assignment = []
+        for i in range(len(polygons)):
+            if i in matches:
+                assignment.append(matches[i])
+            else:
+                assignment.append(-1)
+        return assignment
+                
+
+
+
+
+        
+        
