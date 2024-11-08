1920d564e737dc64c028151690c0b77c5d62b5fe
Julianna Yu <julianna-yu@github.com>
2024-11-08 10:03:37 -0800

    final player

--- players/group10_player.py
+++ players/group10_player.py
@@ -2,6 +2,7 @@ from collections import defaultdict
 import os
 import pickle
 from typing import List
+import pprint
 
 import numpy as np
 import math
@@ -34,6 +35,7 @@ class Player:
         self.cake_width = None
         self.cake_diagonal = None
         self.requests = None
+        self.original_requests = None
         self.cuts = []
         self.base_case_switch = False, None
         self.x_y_switch = True # True: y working height, False: x working height
@@ -51,6 +53,7 @@ class Player:
         self.margin = 0.01
         self.zigzag_cuts = []
         self.zigzag_found = False
+        self.set_assignments = {}
 
     def move(self, current_percept) -> tuple[int, List[int]]:
         """Function which retrieves the current state of the amoeba map and returns an amoeba movement
@@ -73,15 +76,13 @@ class Player:
     
         if self.turn_number == 1:
             # initialize instance variables, sorted requests
+            self.original_requests = current_percept.requests
             self.requests = current_percept.requests
             self.cake_len = current_percept.cake_len
             self.cake_width = current_percept.cake_width
             self.requests.sort(reverse=False)
             if not self.uniform_mode:
-                # print("BEFORE")
                 is_uniform, grid_area = self.if_uniform(current_percept.requests, extra_tol)
-                # print("AFTER")
-                print(is_uniform, grid_area)
                 if is_uniform:
                     self.grid_cut(current_percept, grid_area)
                     self.uniform_mode = True
@@ -109,15 +110,9 @@ class Player:
     
         elif self.uniform_mode:
             if (self.cut_number > len(self.uniform_cuts) - 1):
-                # print("I AM TRYING TO ASSIGN PIECES")
                 assignment = self.assignPolygons(polygons=polygons)
                 return constants.ASSIGN, assignment
-            # print("I AM IN UNIFORM MODE")
-            # print("Turn Number: ", turn_number)
-            # print("Cut Number: ", cut_number)
             if self.turn_number == 1:
-                # print(self.uniform_cuts)
-                # print(self.uniform_cuts[cut_number])
                 return constants.INIT, self.uniform_cuts[self.cut_number]
 
             return constants.CUT, self.uniform_cuts[self.cut_number]
@@ -133,14 +128,18 @@ class Player:
 
             factor_penalty = dict()
             factor_cuts = dict()
+            factor_assignments = dict()
             
             for factor in list_of_factors[::-1]:
-                penalty, cuts = self.simulate_cuts(factor)
+                penalty, cuts, all_assignments = self.simulate_cuts(factor)
                 factor_penalty[tuple(factor)] = penalty
                 factor_cuts[tuple(factor)] = cuts
+                factor_assignments[tuple(factor)] = all_assignments
+
 
             best_factor = min(factor_penalty, key=factor_penalty.get)
             best_cut = factor_cuts[best_factor]
+            self.set_assignments = factor_assignments[best_factor]
             zigzag = []
 
             working_length = self.cake_len - (2 * self.margin)
@@ -177,6 +176,7 @@ class Player:
                     zigzag.append(tuple(self.add_crumbs(temp, False)))
                     zigzag.append((round(float(best_cut[i][0][0]), 2), round(float(best_cut[i][0][1]), 2))) #Top-Coord
                     zigzag.append((round(float(best_cut[i][1][0]), 2), round(float(best_cut[i][1][1]), 2))) #Bot-Coord
+
                 # If odd, bot to top.
                 else:
                     temp = (float(best_cut[i][1][0])), float(best_cut[i][1][1])
@@ -187,8 +187,6 @@ class Player:
             self.zigzag_cuts = zigzag
 
             self.zigzag_found = True
-            # print("self.angle_cuts is: ", self.angle_cuts)
-            # print("best cuts is: ", best_cut)
             return constants.INIT, list(self.zigzag_cuts[0])
     
     # Add crumbs to existing cuts
@@ -209,12 +207,10 @@ class Player:
                 crumb_coord[1] = 0
             else:
                 crumb_coord[1] = self.cake_len
-            print("CHECK IF TARGET [0] = 0,", target[0], type(target[0]))
             if target[0] == 0: #We are at left
                 crumb_coord[0] = 0.01
             else:
                 crumb_coord[0] = round(self.cake_width - 0.01, 2)
-        
         return crumb_coord
                 
 
@@ -223,19 +219,19 @@ class Player:
         total_penalty = 0
         self.working_height = round(float((self.cake_len - (2 * self.margin))/factor[0]), 2)
         horizontals = []
+        all_assignments = {}
         for j in range (factor[0] + 1):
             horizontals.append(round(self.margin + (self.working_height * j), 2))
-        print ("working_height:", self.working_height)
-        print ("horizontals:", horizontals)
         for i in range(factor[1]):
             tolerances = self.find_acceptable_range(factor[0], i) # Returns tolerances of the smaller factors
-            penalty = self.angle_sweep(tolerances, factor[0], i, horizontals)
+            penalty, assignments = self.angle_sweep(tolerances, factor[0], i, horizontals)
             total_penalty += penalty
 
+        all_assignments.update(assignments)
         cut_arr = self.angle_cuts.copy()
         self.angle_cuts = []
 
-        return total_penalty, cut_arr
+        return total_penalty, cut_arr, all_assignments
     
     def calcDiagonal(self) -> float:
         return (math.sqrt((self.cake_len * self.cake_len) + (self.cake_width * self.cake_width)))
@@ -248,17 +244,8 @@ class Player:
 
         
         if self.base_case_switch[0]:
-            # TODO: this currently retraces the past cut and creates triangles from the same point
             x = self.cuts[self.cut_number - 2][0]
             y = self.cuts[self.cut_number - 2][1] # on the first occurrence, this will be 0 if we went from bottom to right and cake_len if we went from top to right
-            print ("x, y: ", x, y)
-            # TODO: make this align with everything else
-            print ("y from two turns ago:", self.cuts[self.cut_number - 2][1])
-            print ("from one turn ago:", self.cuts[self.cut_number - 1][0], self.cuts[self.cut_number - 1][1])
-            print ("adjustment for next area:", 2 * current_area / self.working_height)
-            print ("x_y_switch:", self.x_y_switch)
-            print ("base_case_switch:", self.base_case_switch)
-
             if self.x_y_switch and self.base_case_switch[1]:
                 self.working_height = self.cuts[self.cut_number - 1][1]
                 self.x_y_switch = not self.x_y_switch
@@ -294,8 +281,6 @@ class Player:
             self.working_height = self.cake_width - self.cur_pos[0]
             if (self.cut_number < len(self.requests)): # not on our last request
                 area_left += sum(self.requests[self.cut_number:])
-            print ("area left:", area_left)
-            print ("working height:", self.working_height)
             x = self.cake_width
             if (y == 0): 
                 y = round(self.cake_len - 2 * area_left / self.working_height, 2)
@@ -309,34 +294,31 @@ class Player:
     
     def assignPolygons(self, polygons) -> list[int]:
         # parse polygons to polygon_areas: dict(rank: (area, i))
-        # print(polygons)
         polygon_areas = []
         requests_items = []
         for i in range(len(polygons)):
             polygon_areas.append((polygons[i].area, i)) 
         for i in range(len(self.requests)):
-            requests_items.append((self.requests[i], i))
-        # print("AREAS: ", polygon_areas)
-        # print("REQUESTS: ", requests_items)
+            requests_items.append((self.original_requests[i], i))
+        requests_items.sort()
+        polygon_areas.sort()
 
         matches = {} # request : polygon
-
         for i in range(len(requests_items)):
-            # print(f"I AM ON {i} ITERATION")
-            min_diff = math.inf
+            if requests_items[i][0] in self.set_assignments:
+                matches[i] = self.set_assignments[requests_items[i][0]]
+                continue
+            min_penalty= math.inf
             polygon = -1
             polygon_index = -1
-            # print("INIT COMPLETE")
             for j in range(len(polygon_areas)):
-                # print("area ", polygon_areas[j][0])
-                # print("request ", requests_items[i][0])
-                temp_diff = abs(float(polygon_areas[j][0]) - float(requests_items[i][0]))
-                # print("TEMP DIFF SET")
-                if temp_diff < min_diff:
-                    min_diff = temp_diff
+                temp_penalty = 100 * (abs(float(polygon_areas[j][0]) - float(requests_items[i][0]))/float(requests_items[i][0]))
+                if temp_penalty <= self.tolerance: 
+                    temp_penalty = 0
+                if temp_penalty < min_penalty:
+                    min_penalty = temp_penalty
                     polygon = polygon_areas[j][1]
                     polygon_index = j
-            # print ("polygon:", polygon)
             matches[i] = polygon
             polygon_areas.pop(polygon_index)
         
@@ -346,7 +328,6 @@ class Player:
                 assignment.append(matches[i])
             else:
                 assignment.append(-1)
-        print("THIS IS MY ASSIGNMENT", assignment)
         return assignment
                 
     #Returns whether if it is uniform, and the area of the uniform. If not, area is -1
@@ -358,9 +339,6 @@ class Player:
 
         tolerance = (self.tolerance + extra_tol) / 100
         skewed_average = (1 - tolerance**2)*(sum(requests) / len(requests)) if tolerance < 1 else (tolerance**2)**(sum(requests) / len(requests))
-        print("THIS IS OUR TOLERANCE: ", tolerance)
-        print("THIS IS OUR TRUE AVG: ", sum(requests)/len(requests))
-        print("THIS IS OUR SKEWED AVG: ", skewed_average)
         for req in requests:
             if (abs(req-skewed_average)/req) >= tolerance:
                 return (False, -1)
@@ -369,8 +347,6 @@ class Player:
     
     #Cuts into grid using crumbs method, returns the coordinate to cut to.
     def grid_cut(self, current_percept, grid_area) -> list[float, float]:
-        # print("IN GRID CUT")
-        print(self.cake_len)
         # length_shave = 0.01266028 * self.cake_len
         
         # width_shave = round(1.6 * length_shave, 2)
@@ -379,10 +355,8 @@ class Player:
         width_shave = 0.01
         length_shave = 0.01
 
-        # print("BEFORE FACTORS")
         s_factor, l_factor = self.find_factors(closest=True)
 
-        print("MY FACTORS: ", s_factor, l_factor)
         x_cuts = [width_shave] # first cut
         y_cuts = [length_shave] # first cut
         x_increment = round((self.cake_width - 2*width_shave)/l_factor, 2)
@@ -395,8 +369,7 @@ class Player:
         for ind in range(l_factor):
             x_cuts.append(round(x_cuts[ind] + x_increment, 2))
 
-        print("X CUTS: ", x_cuts)
-        print("Y CUTS: ", y_cuts)
+
 
         for i in range(len(y_cuts)):
             if i == 0:
@@ -444,8 +417,6 @@ class Player:
                     self.uniform_cuts.append([round(self.cake_width - x_cuts[i], 2), 0])
                 
            
-        print("PATH TO VICTORY: ", self.uniform_cuts)
-
     #Finds the closest factor with the number of requests
     def find_factors(self, num_requests, closest: bool) -> list[int, int]: # [smaller_closest_factor, bigger_closest_factor]
         num_of_requests = num_requests
@@ -478,7 +449,6 @@ class Player:
                 upper_area = self.requests[req + iteration*factor] * (1+(self.tolerance/100))
                 
                 # Because the diff between the longer and shorter edges is fixed and certain
-                #print(self.angle_cuts)
                 diff_a_b = abs(self.angle_cuts[iteration - 1][0][0]-self.angle_cuts[iteration - 1][1][0])/factor
 
                 # [(x + x + diff_a_b)/2] * h = area
@@ -493,10 +463,9 @@ class Player:
                     lower_wid = lower_wid - diff_a_b*(req) + self.angle_cuts[iteration - 1][0][0]
                     upper_wid = upper_wid - diff_a_b*(req) + self.angle_cuts[iteration - 1][0][0]
                 temp.append([lower_wid,upper_wid])
-        #print ("tolerances:", temp)
         return temp
 
-    # angle sweep does a search of all possible lines and adds the one with least penalty to the self.angle_cuts variable, returns the penalty
+    # Does a search of all possible lines and adds the one with least penalty to the self.angle_cuts variable, returns the penalty
     """
     self.angle_sweep(tolerances: list[list[lower_tolerance, upper_tolerance]], target_requests: list[requests])
     - using knowledge of the previous cut (from self.angle_cuts), find the next cut that minimizes the penalty for the list of target_requests. 
@@ -509,23 +478,38 @@ class Player:
             (tolerances[0][1], tolerances[-1][1])
         )
         target_reqs = self.requests[iteration * factor : (iteration + 1) * factor]
-        min_penalty = (math.inf, ((0, 0), (0, 0)), 0) # penalty, cuts, slope 
+        min_penalty = (math.inf, ((0, 0), (0, 0)), 0, {}) # penalty, cuts, slope, set_assignments
         for coord1x in np.arange(x_range[0][0], x_range[1][0], 0.01):
+            if coord1x <= 0.01:
+                continue
             for coord2x in np.arange(x_range[0][1], x_range[1][1], 0.01):
+                if coord2x <= 0.01:
+                    continue
+                set_assignments = {}
                 cuts = None
                 penalty = 0
                 coord1 = (coord1x, self.margin + (self.working_height / 2))
                 coord2 = (coord2x, self.cake_len - (self.margin + (self.working_height / 2)))
                 slope, intercept = self.equation_of_line(coord1, coord2)
                 if not slope:
-                    cuts = (
+                    cuts = self.rounded((
                         (coord1[0], 0), (coord1[0], self.cake_len)
-                    )
+                    ))
                 else:
-                    cuts = (
+                    cuts = self.rounded((
                         self.point_on_line(slope, intercept, 0, False), # coordinate for top of cake
                         self.point_on_line(slope, intercept, -1 * self.cake_len, False) # coordinate for bottom of cake
-                    )
+                    ))
+                
+                # CHECK IF THIS LINE RUNS INTO THE LAST ONE
+                if self.angle_cuts:
+                    last_cuts = self.angle_cuts[-1] # tuple[tuple, tuple]
+                    # checking top coordinates
+                    if (last_cuts[0][0] > cuts[0][0]):
+                        continue
+                    elif (last_cuts[1][0] > cuts[1][0]):
+                        continue
+
                 # check penalty for all other requests 
                 for i in range(factor):
                     req = target_reqs[i]
@@ -535,13 +519,18 @@ class Player:
                         elif coord1x > tolerances[i][1]:
                             penalty += 100 * ((coord1x - tolerances[i][1]) * self.working_height) / req
                     else:
-                        penalty += self.calculate_penalty(req, slope, intercept, (horizontals[i], horizontals[i + 1]))
-                if penalty == min_penalty[0] and not slope:
-                    min_penalty = (penalty, cuts, math.inf)
+                        curr_penalty = self.calculate_penalty(req, slope, intercept, (horizontals[i], horizontals[i + 1]))
+                        if curr_penalty > 100:
+                            set_assignments[req] = -1
+                            penalty += 100
+                        else:
+                            penalty += curr_penalty
+                if penalty == min_penalty[0] and not slope and cuts[0][0] + cuts[1][0] <= min_penalty[1][0][0] + min_penalty[1][1][0]:
+                    min_penalty = (penalty, cuts, math.inf, set_assignments)
                 elif penalty < min_penalty[0] or (penalty == min_penalty[0] and abs(slope) > min_penalty[2]):
-                    min_penalty = (penalty, cuts, slope)
+                    min_penalty = (penalty, cuts, slope, set_assignments)
         self.angle_cuts.append(min_penalty[1])
-        return min_penalty[0]
+        return min_penalty[0], min_penalty[3]
         
     def equation_of_line(self, coord1: tuple, coord2: tuple) -> tuple[bool|float, float]:
         # putting cake coordinates on a cartesian plane, all y coordinates are negated
@@ -572,7 +561,6 @@ class Player:
             else:
                 corner3 = self.point_on_line(slope, intercept, -1 * y_bounds[0], False)
                 corner4 = self.point_on_line(slope, intercept, -1 * y_bounds[1], False)
-        #print ("corners:", corner3, corner1, corner2, corner4)
         x = Polygon([corner3, corner1, corner2, corner4, corner3])
         diff = 100 * abs((req - x.area) / req)
         if diff < self.tolerance:
@@ -594,3 +582,54 @@ class Player:
 
         return res["radius"] <= 12.5
     
+    def rounded(self, cuts: tuple[tuple, tuple]) -> tuple[tuple, tuple]:
+        return (
+            (round(cuts[0][0], 2), round(cuts[0][1], 2)),
+            (round(cuts[1][0], 2), round(cuts[1][1], 2))
+                )
+    
+    def major_outliers(self):
+        q1 = np.percentile(self.requests, 25)
+        q3 = np.percentile(self.requests, 75)
+        iqr = q3 - q1
+        upper = q3 + 1.5 * iqr
+        lower = q1 - 1.5 * iqr
+        new_requests = []
+        outlier_requests = []
+        for req in self.requests:
+            if req < lower or req > upper: 
+                outlier_requests.append(req)
+            else:
+                new_requests.append(req)
+        return new_requests, outlier_requests
+        
+    
+    def find_outliers(self, factor: tuple[int, int]):
+        outlier_count = 0
+        new_skew = 0
+        i = 0
+        
+        # each iteration (larger factor)
+        while i * factor[1] + new_skew < factor[0] * factor[1]:
+            # each request in each iteration (smaller factor)
+            outlier_lists = {} # count: outliers
+            least_outliers = None # outlier_count w the least interference
+            while outlier_count == 0 or (len(outlier_req) > 0.5 * factor[0] and outlier_count % factor[0] != 0):
+                outlier = False
+                outlier_req = []
+                outlier_count += 1
+                target_reqs = self.requests[i * factor[0] + new_skew + outlier_count : (i + 1) * factor[0] + new_skew + outlier_count]
+                first = target_reqs[outlier_count]
+                for j in range(outlier_count + 1, factor[0]):
+                    if target_reqs[j] / first > (j + 1) ** 2 or target_reqs[j] / first < (j + 1) ** -2:
+                        outlier = True
+                        outlier_req.append(target_reqs[j])
+                outlier_lists[outlier_count] = outlier_req
+                least_outliers = min(outlier_count, key=len(outlier_lists.get))
+            new_skew = least_outliers
+        return 0
+
+            
+            
+
+            
